7fd4f3b8a66078026bc3a68bd1a85c0fd4d04a1a
BestFraz <42387042+BestFraz@users.noreply.github.com>
2018-10-09 17:39:33 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+
+var response = ["Fook offfff", "Beeee gone u twaaaat", "Foookn Coont", "Doooowwwnie Scoom", "Shoooot UUUUUp Beeech"];
+
+bot.on('message', (message) => {
+    if (message.author.id == '498937355718361088') {
+       message.reply(response[Math.floor(Math.random() * 5)]);
+    }
+    else{
+        return;
+    }
+
+
+    if (message.content == 'id'){
+        message.reply("Your Id: "+message.author.id+"");
+    }
+
+});
+
+
+ 
+
+bot.login('process.env.BOT_TOKEN');
